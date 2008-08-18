e3075a5f505b9bc6dc34e6d1f0f31026fec948da
universal <jhed@informatik.uni-kiel.de>
2008-08-18 16:50:20 +0200

    added basic import code

--- lib/reify_csv.rb
+++ lib/reify_csv.rb
@@ -28,11 +28,22 @@ module ReifyCSV
 #  end
   
   module ClassMethods
+    # add it to associations where suitable, has_many
+  
     # make it accepting find options
     # make it possible to include has_one/belongs_to associations
-    def to_csv
+    def to_csv(*args)
+      options = args.pop
+      options ||= {}
+      columns = rcsv_parse_export_options(options)
+      # remove select option for now, might bork 
+      find_options = options.except :columns, :select
+      args << find_options
+#     properly escape the fields probably
+#      args << find_options.merge({:select => columns.join(", ")})
       csv_string = FasterCSV.generate do |csv|
-        objects = self.find(:all)
+        csv << self.csv_columns
+        objects = self.find(:all, *args)
         objects.each do |object|
           data = []
           self.csv_columns.each do |col|
@@ -43,16 +54,77 @@ module ReifyCSV
       end
       csv_string
     end
+  
+    # implement
+    # make it play nicely, similarly to to_csv
+    # simulate option, where it just checks the data
+    # possibly add create_from_csv
+    def update_from_csv(csv_src, *args)
+      options = args.pop
+      options ||= {}
+      headers, find_by, simulate, file = rcsv_parse_import_options(options)
+      puts "file? #{file}"
+      errors = Hash.new([])
+      if file
+        data = FasterCSV.read(csv_src, :headers => headers)
+      else
+        data = FasterCSV.parse(csv_src, :headers => headers)
+      end
+      if headers
+        # check if to_s is necessary
+        raise(ArgumentError, "'Find by' columns specified are not included in the actual data.") if find_by.any?{|col| !data.headers.include?(col.to_s)}
+        raise(ArgumentError, "'Find by' columns are not model attributes") if find_by.any?{|col| !column_names.include?(col.to_s) }
+        i = 0
+        
+        finder = "find_by_" + find_by.map{|col| col.to_s}.join("_and_")
+        data_columns = (data.headers - find_by.map{|col| col.to_s}).reject{|col| !column_names.include?(col.to_s)}        
+        data.each do |row|
+          if updateable = send(finder, *find_by.map{|col| row[col.to_s]})
+            data_columns.each {|col| updateable[col] = row[col]}
+            if updateable.valid?
+              updateable.save unless simulate
+            else
+              errors[:invalid] << row
+            end
+          else
+            errors[:not_found] << row
+            # record not found ;)
+            # TODO add handling for that here, probably creation of a new record?
+          end
+        end
+      else
+        # without headers, TODO...
+      end
+      [data.size, errors]
+    end
+
+    def rcsv_parse_export_options(options) #:nodoc:
+      raise ArgumentError, 'parameter hash expected' unless options.respond_to? :symbolize_keys
+      options = options.symbolize_keys
+     # raise ArgumentError, ':page parameter required' unless options.key? :page
+      
+  #    if options[:count] and options[:total_entries]
+  #      raise ArgumentError, ':count and :total_entries are mutually exclusive'
+  #    end
+
+      columns = options[:columns] || self.csv_columns
+  #    per_page = options[:per_page] || self.per_page
+  #    total = options[:total_entries]
+      columns
+    end
+    
+    def rcsv_parse_import_options(options) #:nodoc:
+      raise ArgumentError, 'parameter hash expected' unless options.respond_to? :symbolize_keys
+      options = options.symbolize_keys
+      headers = options.include?(:headers) ? options[:headers] : true
+      find_by = options.include?(:find_by) ? options[:find_by] : [:id]
+      simulate = options.include?(:simulate) ? options[:simulate] : false
+      file = options.include?(:file) ? options[:file] : true
+      [headers, find_by, simulate, file]
+    end
+  
   end
   
-  # implement
-  # make it play nicely, similarly to to_csv
-  # simulate option, where it just checks the data
-  # possibly add create_from_csv
-  def update_from_csv
-    puts "fiibbb"
-  end
-
 #def self.check_csv_update_data(csv_src) 
 #    associated = get_associated.collect{|a| [a.class_name.constantize, a.name]}
 #    data = FasterCSV.read(csv_src, :headers => true)
