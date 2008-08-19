79cfab0c9d3a96af4b0372bfdb1d25015c477608
universal <jhed@informatik.uni-kiel.de>
2008-08-19 19:15:12 +0200

    extended fastercsv extension of the Array class for two special cases: 1. for an array of ActiveRecord::Base objects, here we will call generate_line on each object 2. for an array of arrays, here the inner arrays are passed to a FasterCSV.generate block as lines

---
+++ lib/hooks.rb
@@ -0,0 +1,20 @@
+class Array
+  # Equivalent to <tt>FasterCSV::generate_line(self, options)</tt>.
+  def to_csv(options = Hash.new)
+    raise ArgumentError, 'parameter hash expected' unless options.respond_to? :symbolize_keys
+    options = options.symbolize_keys
+    if self.all?{|element| element.kind_of? ActiveRecord::Base}
+      has_cols = options.has_key? :columns
+      cols = options[:columns]
+      FasterCSV.generate(options.except :columns) do |csv|
+        self.each{|element| csv << (has_cols ? element.generate_line({:columns => cols}) : element.generate_line)}
+      end
+    elsif self.all?{|element| element.kind_of? Array}
+      FasterCSV.generate(options) do |csv|
+        self.each{|element| csv << element}
+      end
+    else
+      FasterCSV.generate_line(self, options)    
+    end
+  end
+end
