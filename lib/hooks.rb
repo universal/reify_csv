5dbca65bad7a63eb78de2cc47bc0fc10dd458d94
universal <jhed@informatik.uni-kiel.de>
2008-09-09 19:26:56 +0200

    added headers to array hook... still needs improvement

--- lib/hooks.rb
+++ lib/hooks.rb
@@ -1,13 +1,14 @@
 class Array
+  # TODO compare to rails Hash.to_xml function, make it more similar
   # Equivalent to <tt>FasterCSV::generate_line(self, options)</tt>.
   def to_csv(options = Hash.new)
     raise ArgumentError, 'parameter hash expected' unless options.respond_to? :symbolize_keys
     options = options.symbolize_keys
     if self.all?{|element| element.kind_of? ActiveRecord::Base}
-      has_cols = options.has_key? :columns
-      cols = options[:columns]
+      cols = options[:columns] || self.first.class.csv_columns
       FasterCSV.generate(options.except(:columns)) do |csv|
-        self.each{|element| csv << (has_cols ? element.generate_line({:columns => cols}) : element.generate_line)}
+        csv << cols
+        self.each{|element| csv << element.generate_line({:columns => cols})}
       end
     elsif self.all?{|element| element.kind_of? Array}
       FasterCSV.generate(options) do |csv|
